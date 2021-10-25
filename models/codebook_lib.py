0a1230a731056b2219ca37230ed43e2c02c8b55d
sylqiu <sylvesterqiu@gmail.com>
2021-10-25 14:56:11 +0800

    initial commit

---
+++ models/codebook_lib.py
@@ -0,0 +1,86 @@
+from typing import Tuple
+import torch
+from torch import nn
+import numpy as np
+
+
+class QuantizeEMA(nn.Module):
+
+  def __init__(self,
+               device: torch.Device,
+               dim: int,
+               n_embed: int,
+               decay: float = 0.99,
+               init_mean: float = 0,
+               init_std: float = 1):
+    """ 
+    Args:
+      device: torch device.
+      dim: The dimension of the code.
+      n_embed: Total number of codes in the code book.
+      decay: Exponential moving average decay.
+      init_mean: The mean of initialization distribution.
+      init_std: The standard deviation of the initialization distribution.
+    """
+    super().__init__()
+
+    self.dim = dim
+    self.init_decay = decay
+    self.n_embed = n_embed
+    self.decay = torch.ones([n_embed]).to(device) * decay
+    self.eps = 1e-5
+    self._device = device
+
+    embed = torch.randn(dim, n_embed) * init_std + init_mean
+    # the code book variable
+    self.register_buffer('embed', embed)
+
+    # record the number of features that corresponds to each code and their
+    # averages.
+    self.register_buffer('cluster_size', torch.ones(n_embed))
+    self.register_buffer('embed_avg', embed.clone())
+
+  def embed_code(self, embed_id: torch.Tensor):
+    return nn.functional.embedding(embed_id, self.embed.transpose(0, 1))
+
+  def forward(
+      self,
+      inputs: torch.Tensor,
+      training: bool = True) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Args:
+      inputs: The feature tensor of shapae (B, C, H, W) where each spatial
+        feature will be quantized.
+      training: torch training argument, during training the code book will be 
+        updated by exponential moving average.
+
+    Return:
+      The quantized feaure (code), the difference between feature and quantized
+      feature, the index of the code in the code book.
+    """
+    inputs = inputs.permute(0, 2, 3, 1)
+    flatten = inputs.reshape(-1, self.dim)
+
+    # compute distance table
+    dist = (flatten.pow(2).sum(1, keepdim=True) - 2 * flatten @ self.embed +
+            self.embed.pow(2).sum(0, keepdim=True))
+    _, embed_ind = (-dist).max(1)
+    embed_onehot = nn.functional.one_hot(embed_ind, self.n_embed).type(flatten.dtype)
+    embed_ind = embed_ind.view(*inputs.shape[:-1])
+    quantize = self.embed_code(embed_ind)
+
+    if training:
+      self.cluster_size.data.mul_(self.decay).add_(
+          embed_onehot.sum(0).mul_(1 - self.decay))
+      embed_sum = flatten.transpose(0, 1) @ embed_onehot
+      self.embed_avg.data.mul_(self.decay).add_(embed_sum.mul_(1 - self.decay))
+      n = self.cluster_size.sum()
+      cluster_size = ((self.cluster_size + self.eps) /
+                      (n + self.n_embed * self.eps) * n)
+      embed_normalized = self.embed_avg / cluster_size.unsqueeze(0)
+      self.embed.data.copy_(embed_normalized)
+
+    diff = quantize.detach() - inputs
+    quantize = inputs + (quantize - inputs).detach()
+    quantize = quantize.permute(0, 3, 1, 2)
+    return quantize, diff, embed_ind
---
+++ models/module_lib.py
@@ -0,0 +1,98 @@
+from typing import Any, Dict, Optional, Sequence
+import torch
+import torch.nn as nn
+from .layer_lib import Conv2DReLUNorm, DownSample2D, UpSample2D
+
+
+class UnetEncoder(nn.Module):
+
+  def __init__(
+      self,
+      input_channels: int,
+      channels_list: Sequence[int],
+      kernel_size_list: Sequence[int],
+      downsample_list: Sequence[bool],
+      output_level_list: Sequence[bool],
+      normalization_config: Dict[str, Any],
+  ):
+    self._layers = []
+    for layer_index in range(len(kernel_size_list)):
+      output_channels = channels_list[layer_index]
+      self._layers.append(
+          Conv2DReLUNorm(input_channels, output_channels,
+                         kernel_size_list[layer_index], **normalization_config))
+      if downsample_list[layer_index]:
+        self._layers.append(DownSample2D(downsample_scale=2))
+      input_channels = output_channels
+
+    self._output_level_list = output_level_list
+
+  def forward(self, x: torch.Tensor) -> Sequence[torch.Tensor]:
+    return_list = []
+    output = x
+    for layer_index, layer in enumerate(self._layers):
+      output = layer(x)
+      if self._output_level_list[layer_index]:
+        return_list.append(output)
+
+    return return_list[::-1]
+
+
+class UnetDecoder(nn.Module):
+
+  def __init__(
+      self,
+      input_channels: int,
+      channels_list: Sequence[int],
+      kernel_size_list: Sequence[int],
+      skip_channels_list: Sequence[Optional[int]],
+      output_level_list: Sequence[bool],
+      normalization_config: Dict[str, Any],
+  ):
+    self._layers = []
+    self._output_level_list = output_level_list
+    for layer_index in range(len(kernel_size_list)):
+      output_channels = channels_list[layer_index]
+      if skip_channels_list[layer_index] is not None:
+        input_channels += skip_channels_list[layer_index]
+        self._layers.append((
+            UpSample2D(),
+            Conv2DReLUNorm(input_channels, output_channels,
+                           kernel_size_list[layer_index],
+                           **normalization_config),
+        ))
+      else:
+        self._layers.append(
+            Conv2DReLUNorm(input_channels, output_channels,
+                           kernel_size_list[layer_index],
+                           **normalization_config))
+    input_channels = output_channels
+
+  def forward(self, x: Sequence[torch.Tensor]):
+    skip_index = 1
+    outputs = x[0]
+    outputs_list = []
+    for level_index in range(len(self._output_level_list)):
+      if isinstance(self._layers[level_index], tuple):
+        # has upsample layer if and only if has skip connection,
+        # except when it is the bottom-most feature.
+        upsample_layer, conv_layer = self._layers[level_index]
+        skip_inputs = x[skip_index]
+        outputs = torch.cat((upsample_layer(outputs, skip_inputs.shape[2:4])),
+                            axis=1)
+        outputs = conv_layer(outputs)
+        skip_index += 1
+      else:
+        conv_layer = self._layers[level_index]
+        outputs = conv_layer(outputs)
+
+      if self._output_level_list[level_index]:
+        outputs_list.append(outputs)
+
+    return outputs_list
+    
+  
+    
+    
+      
+    
