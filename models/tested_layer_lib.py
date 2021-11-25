8caa7fea5e204ef320107314aa0141812ae4896f
Yuchen <42129540+guo1228@users.noreply.github.com>
2021-11-25 15:23:30 +0800

    Already tested all layers

---
+++ models/tested_layer_lib.py
@@ -0,0 +1,183 @@
+from typing import Any, Dict, Optional, Sequence, Tuple
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+
+def get_normalization_config(activation: Optional[str], use_bias: bool,
+                             use_batchnorm: bool):
+  return {
+      "activation": activation,
+      "use_bias": use_bias,
+      "use_batchnorm": use_batchnorm,
+  }
+
+
+def get_activation_layer(activation: str):
+  if activation == "relu":
+    return torch.nn.ReLU(inplace=True)
+  else:
+    raise NotImplementedError
+
+
+class Conv2DReLUNorm(torch.nn.Module):
+
+  def __init__(
+      self,
+      input_dim: int,
+      output_dim: int,
+      kernel_size: int,
+      activation: Optional[str],
+      use_bias: bool = True,
+      use_batchnorm: bool = False,
+  ):
+
+    super().__init__()
+    if activation is None:
+      self._activation = None
+    else:
+      self._activation = get_activation_layer(activation)
+
+    self._bn = None
+    if use_batchnorm:
+      use_bias = False
+      self._bn = torch.nn.BatchNorm2d(output_dim)
+
+    padding = kernel_size // 2
+    self._conv = torch.nn.Conv2d(
+        input_dim,
+        output_dim,
+        kernel_size=kernel_size,
+        padding=padding,
+        bias=use_bias,
+    )
+
+  def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    outputs = self._conv(inputs)
+    if self._activation is not None:
+      outputs = self._activation(outputs)
+    if self._bn is not None:
+      outputs = self._bn(outputs)
+
+    return outputs
+
+
+class DownSample2D(torch.nn.Module):
+
+  def __init__(self, downsample_scale: int, mode: str):
+    super().__init__()
+    self._downsample_scale = downsample_scale
+    self._mode = mode
+
+  def forward(self,
+              inputs: torch.Tensor,
+              target_shape: Tuple[int, int] = None) -> torch.Tensor:
+    if target_shape is None:
+      target_shape = (
+          inputs.shape[2] // self._downsample_scale,
+          inputs.shape[3] // self._downsample_scale,
+      )
+    outputs = torch.nn.functional.interpolate(inputs,
+                                        mode=self._mode,
+                                        size=target_shape,
+                                        align_corners=True)
+    return outputs
+
+
+class UpSample2D(torch.nn.Module):
+
+  def __init__(self,
+               upsample_scale: Optional[int] = None,
+               mode: str = "bilinear"):
+    super().__init__()
+    self._upsample_scale = upsample_scale
+    self._mode = mode
+
+  def forward(self,
+              inputs: torch.Tensor,
+              target_shape: Tuple[int, int] = None) -> torch.Tensor:
+    if target_shape is None and self._upsample_scale is not None:
+      target_shape = (
+          inputs.shape[2] * self._upsample_scale,
+          inputs.shape[3] * self._upsample_scale,
+      )
+    outputs = torch.nn.functional.interpolate(inputs,
+                                        mode=self._mode,
+                                        size=target_shape,
+                                        align_corners=True)
+    return outputs
+
+
+class ResidualBlock(torch.nn.Module):
+
+  def __init__(
+      self,
+      channels: int,
+      kernel_size_list: Sequence[int],
+      normalization_config: Dict[str, Any],
+  ):
+    super().__init__()
+    self._conv_layers = []
+    self._norm_layers = []
+    use_bias = normalization_config['use_bias']
+    use_bn = normalization_config['use_batchnorm']
+    if normalization_config['activation'] is None:
+      self._activation = None
+    else:
+      self._activation = get_activation_layer(
+          normalization_config['activation'])
+
+    for kernel_size in kernel_size_list:
+      padding = kernel_size // 2
+      self._conv_layers.append(
+          torch.nn.Conv2d(
+              channels,
+              channels,
+              kernel_size,
+              padding=padding,
+              bias=use_bias,
+          ))
+      if use_bn:
+        self._norm_layers.append(torch.nn.BatchNorm2d(channels))
+      else:
+        self._norm_layers.append(None)
+
+  def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    outputs = inputs
+    for layer, norm in zip(self._conv_layers, self._norm_layers):
+      res = layer(outputs)
+      if self._activation is not None:
+        res = self._activation(res)
+      if norm is not None:
+        res = norm(res)
+
+      outputs += res
+
+    return outputs
+
+
+if __name__ == '__main__':
+  '''
+  cov = Conv2DReLUNorm(3, 5, 5, activation = None)
+  matrix = torch.randn(1, 3, 32, 32)
+  output = cov(matrix)
+  print(output.shape)
+  '''
+  '''
+  down = DownSample2D(2, 'bilinear')
+  matrix = torch.randn(1, 3, 32, 32)
+  output = down(matrix, target_shape = None)
+  print(output.shape)
+  '''
+  '''
+  upsam = UpSample2D(2)
+  matrix = torch.randn(1, 3, 32, 32)
+  output = upsam(matrix, target_shape = None)
+  print(output.shape)
+  '''
+  '''
+  res = ResidualBlock(3, [3, 3], {'use_batchnorm':1, 'use_bias':0, 'activation':'relu'})
+  matrix = torch.randn(1, 3, 32, 32)
+  output = res(matrix)
+  print(output.shape)
+  '''
