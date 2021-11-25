b460bf455e82b9f8374132aa94349e4646257d34
gli <gli@pclan.math.cuhk.edu.hk>
2021-11-25 16:33:28 +0800

    Implement layer_lib unit test

--- models/layer_lib_test.py
+++ models/layer_lib_test.py
@@ -1,6 +1,6 @@
 import unittest
 import torch
-import layer_lib
+import tested_layer_lib as layer_lib
 
 HAS_CUDA = torch.cuda.is_available()
 BATCHSIZE = 2
@@ -8,6 +8,10 @@ HEIGHT = 10
 WIDTH = 20
 INPUT_CHANNELS = 3
 OUTPUT_CHANNELS = 12
+DOWNSAMPLE_SCALE = 2
+UPSAMPLE_SCALE = 2
+KERNEL_SIZE_LIST = [3, 3]
+
 
 
 class TestLayerLib(unittest.TestCase):
@@ -20,6 +24,33 @@ class TestLayerLib(unittest.TestCase):
 
     self.assertSequenceEqual(outputs.shape,
                              (BATCHSIZE, OUTPUT_CHANNELS, HEIGHT, WIDTH))
+  
+  def test_DownSample2D(self):
+    inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+    layer = layer_lib.DownSample2D(DOWNSAMPLE_SCALE, 'bilinear')
+    outputs = layer(inputs)
+
+    self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, INPUT_CHANNELS, HEIGHT//DOWNSAMPLE_SCALE, WIDTH//DOWNSAMPLE_SCALE))
+
+  def test_UpSample2D(self):
+    inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+    layer = layer_lib.UpSample2D(UPSAMPLE_SCALE)
+    outputs = layer(inputs)
+
+    self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, INPUT_CHANNELS, HEIGHT*UPSAMPLE_SCALE, WIDTH*UPSAMPLE_SCALE))
+  
+  def test_ResidualBlock(self):
+    inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+    layer = layer_lib.ResidualBlock(INPUT_CHANNELS, KERNEL_SIZE_LIST, {'use_batchnorm':1, 'use_bias':0, 'activation':'relu'})
+    outputs = layer(inputs)
+
+    self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH))
 
 
 if __name__ == '__main__':
