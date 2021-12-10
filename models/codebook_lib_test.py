b7aead43972aaf2b3c6cc8bf71f017c575abb9a9
sylqiu <sylvesterqiu@gmail.com>
2021-12-10 16:54:18 +0800

    Revert "Revert "Revert "Revert "finished test""""

---
+++ models/codebook_lib_test.py
@@ -0,0 +1,26 @@
+import unittest
+import torch
+import codebook_lib
+
+
+dim = 4
+n_embed = 10
+BATCHSIZE = 2
+HEIGHT = 11
+WIDTH = 15
+INPUT_CHANNELS = 4
+
+class TestCodebookLib(unittest.TestCase):
+    def test_quantizeema(self):
+        inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = codebook_lib.QuantizeEMA(dim, n_embed)
+        outputs = layer(inputs)
+
+        self.assertSequenceEqual([outputs[0].shape, outputs[1].shape, outputs[2].shape],
+                             [(BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH), 
+                             (BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH), 
+                             (BATCHSIZE, HEIGHT, WIDTH)])
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ models/model_lib_test.py
@@ -0,0 +1,155 @@
+import unittest
+import torch
+import model_lib
+import sys
+#sys.path.append('C:/Users/ligen/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline')
+sys.path.append('C:/Users/gli/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline')
+import configure_param
+import gin
+
+gin.parse_config_file('C:/Users/gli/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/common.gin')
+gin.parse_config_file('C:/Users/gli/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/discrete.gin')
+gin.parse_config_file('C:/Users/gli/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/gaussian.gin')
+#gin.parse_config_file('C:/Users/ligen/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/common.gin')
+#gin.parse_config_file('C:/Users/ligen/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/discrete.gin')
+#gin.parse_config_file('C:/Users/ligen/OneDrive - The Chinese University of Hong Kong/Code/MUE/pipeline/configs/gaussian.gin')
+
+BATCHSIZE = 2
+HEIGHT = 256
+WIDTH = 256
+INPUT_CHANNELS = 3
+
+LABEL_CHANNELS = 1
+FEATURE_CHANNLES = 2
+
+LATENT_CODE_DIMENSION = 12
+
+OUTPUT_CHANNELS = 2
+CODE_BOOK_SIZE = 10
+NORMALIZATION_CONFIG = {'use_batchnorm':1, 'use_bias':0, 'activation':'relu'}
+
+
+
+class TestModelLib(unittest.TestCase):
+
+    def test_label_combination_layer(self):
+        inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        labels = torch.ones((BATCHSIZE, LABEL_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = model_lib.LabelCombinationLayer(INPUT_CHANNELS, LABEL_CHANNELS, FEATURE_CHANNLES)
+        outputs = layer(inputs, labels)
+        self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, FEATURE_CHANNLES, HEIGHT, WIDTH))
+
+    def test_latent_combination_layer(self):
+        latent_code = torch.ones((BATCHSIZE, LATENT_CODE_DIMENSION, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        feature = torch.ones((BATCHSIZE, FEATURE_CHANNLES, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = model_lib.LatentCombinationLayer(LATENT_CODE_DIMENSION, FEATURE_CHANNLES)
+        outputs = layer(latent_code, feature)
+        self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, FEATURE_CHANNLES, HEIGHT, WIDTH))
+
+    def test_latent_code_classifier(self):
+        inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = model_lib.LatentCodeClassifier(INPUT_CHANNELS, OUTPUT_CHANNELS, 
+                             CODE_BOOK_SIZE, NORMALIZATION_CONFIG)
+        outputs = layer(inputs)
+        self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, CODE_BOOK_SIZE))
+
+    def test_gaussian_encoder(self):
+        inputs = torch.ones((BATCHSIZE, INPUT_CHANNELS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        PARAM = configure_param.get_gaussian_encoder_param(INPUT_CHANNELS)
+        UNET_PARAM = PARAM["unet_encoder_param"]
+        KERNEL_SIZE_LIST = UNET_PARAM["kernel_size_list"]
+        ENCODER_CHANNEL_LIST = UNET_PARAM["channels_list"]
+        DOWNSAMPLE_LIST = UNET_PARAM["downsample_list"]
+        OUTPUT_LEVEL_LIST = UNET_PARAM["output_level_list"]
+        layer = model_lib.GaussianEncoder(**PARAM)
+        outputs = layer(inputs)
+        outputs = outputs[::-1]
+        k = 0
+        h = HEIGHT
+        w =WIDTH
+        for i in range(len(KERNEL_SIZE_LIST), 2):
+                self.assertSequenceEqual(outputs[k].shape,
+                             (BATCHSIZE, ENCODER_CHANNEL_LIST[i], h, w))
+                k = k+1
+                h = h//2
+                w = w//2 
+                    
+
+
+
+    def test_discrete_posterior_encoder(self):
+        
+        LATENT_CODE_DIMENSIONS = 128
+        PARAM = configure_param.get_discrete_posterior_encoder_param(LATENT_CODE_DIMENSIONS)
+        inputs = torch.ones((BATCHSIZE, LATENT_CODE_DIMENSIONS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = model_lib.DiscretePosteriorEncoder(**PARAM)
+        layer.initialize_code_book(0, 1)
+        outputs = layer(inputs)
+        self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, LATENT_CODE_DIMENSIONS, 1, 1))
+
+
+    def test_discrete_prior_encoder(self):
+        LATENT_CODE_DIMENSIONS = 12
+        PARAM = configure_param.get_discrete_prior_encoder_param(LATENT_CODE_DIMENSIONS)
+        inputs = torch.ones((BATCHSIZE, LATENT_CODE_DIMENSIONS, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        layer = model_lib.DiscretePriorEncoder(**PARAM)
+        layer.get_code_book(torch.ones((128,512), dtype=torch.float32))
+        outputs = layer(inputs)
+        UNET_PARAM = PARAM["unet_encoder_param"]
+        KERNEL_SIZE_LIST = UNET_PARAM["kernel_size_list"]
+        ENCODER_CHANNEL_LIST = UNET_PARAM["channels_list"]
+        OUTPUT_LEVEL_LIST = UNET_PARAM["output_level_list"]
+        outputs = outputs[::-1]
+        k = 0
+        h = HEIGHT
+        w =WIDTH
+        for i in range(len(KERNEL_SIZE_LIST), 2):
+                self.assertSequenceEqual(outputs[k].shape,
+                             (BATCHSIZE, ENCODER_CHANNEL_LIST[i], h, w))
+                k = k+1
+                h = h//2
+                w = w//2
+    
+    def test_conditional_vae(self):
+        inputs = torch.ones((BATCHSIZE, 1, HEIGHT, WIDTH),
+                        dtype=torch.float32)
+        labels = torch.ones((BATCHSIZE, 1, HEIGHT, WIDTH),
+                        dtype=torch.float32)                
+        PARAM = configure_param.get_cvae_param()
+        layer = model_lib.ConditionalVAE(**PARAM)
+        outputs = layer(inputs, labels)
+        self.assertSequenceEqual(outputs.shape,
+                             (BATCHSIZE, 1, HEIGHT, WIDTH))
+
+        infer = layer.inference(inputs)
+        self.assertSequenceEqual(infer[0][0].shape,
+                             (BATCHSIZE, 1, HEIGHT, WIDTH))
+        print(infer[1:])
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  unittest.main()
